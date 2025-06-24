5900c09af025eb6c2a1ebaba218b23cc7b0c2868
rojojun <zoloman316@gmail.com>
2025-06-24 21:54:50 +0900

    Chapter2 함수형 스택

--- src/test/kotlin/exercise2/Question1.kt
+++ src/test/kotlin/exercise2/Question1.kt
@@ -1,7 +1,36 @@
 package exercise2
 
-val processFun = ::extractListData andThen
-        ::
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import kotlin.test.Test
 
 class Question1 {
+    @Test
+    fun `push into the stack`() {
+        val stack1 = FunStack<Char>()
+        val stack2 = stack1.push('A')
+        expectThat(stack1.size()).isEqualTo(0)
+        expectThat(stack2.size()).isEqualTo(1)
+    }
+
+    @Test
+    fun `push push pop`() {
+        val (b, stack) = FunStack<Char>()
+            .push('A')
+            .push('B')
+            .pop()
+
+        expectThat(stack.size()).isEqualTo(1)
+        expectThat(b).isEqualTo('B')
+    }
+}
+
+data class FunStack<T>(
+    private val elements: List<T> = emptyList(),
+) {
+    fun push(element: T): FunStack<T> = FunStack(listOf(element) + elements)
+
+    fun pop(): Pair<T, FunStack<T>> = elements.first() to FunStack(elements.drop(1))
+
+    fun size() = elements.size
 }
